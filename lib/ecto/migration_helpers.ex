b59af0b1ed88db6c6f65256362df181266b8bfb7
Paul Balomiri <paulbalomiri@gmail.com>
2016-12-19 12:19:28 +0200

    Added drop_from_create_type Modified drop_from_create to ignore `CREATE TYPE`

--- lib/ecto/migration_helpers.ex
+++ lib/ecto/migration_helpers.ex
@@ -8,9 +8,20 @@ defmodule OA.Ecto.MigrationHelpers do
   def drop_from_create(func_def) do
     func_def
     |> String.replace(~r/RETURNS.*/s, "") # drops function body
-    |> String.replace( ~r/CREATE/s , "DROP FUNCTION" ) #replace create with drop
-    |> String.replace(~r/\s+OR\s+REPLACE\s+FUNCTION/s , " IF EXISTS") # if a OR REPLACE existed then generate an IF EXISTS DROP
+    |> String.replace( ~r/CREATE\s+(OR\s+REPLACE)\s+FUNCTION/s , "DROP FUNCTION \\g{1}" ) #replace create with drop
+    |> String.replace(~r/\s+OR\s+REPLACE/s , " IF EXISTS") # if a OR REPLACE existed then generate an IF EXISTS DROP
     |> String.replace(~r/.*DROP/s, "DROP")
     |> String.replace(~r/(DEFAULT|:=)[^,)]+/s, "") # Also remove default values in `field type DEFAULT default` and in `field type := default` notation
+  end
+
+  def drop_from_create_type(type_def) do
+    ret =
+      type_def
+      |> String.replace( ~r/.*CREATE\s+TYPE\s+([^([:space:]]+).*/s , "DROP TYPE \\g{1}" )
+    if ret == type_def do
+      nil
+    else
+      ret
+    end
   end
 end
