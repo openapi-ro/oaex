56df897f4282724d2b5564d8f243c1afa4408f35
Paul Balomiri <paulbalomiri@gmail.com>
2017-04-21 17:41:19 +0300

    added `endure_atom/1` and `ensure_existing_atom/1`

---
+++ lib/string.ex
@@ -0,0 +1,25 @@
+defmodule OA.String do
+  @doc """
+    returns
+    - the atom if `parameter` is already an atom
+    - `String.to_atom(parameter)` if it is a `bitstring`
+  """
+  def ensure_atom( string) when is_bitstring(string) do
+    String.to_atom string
+  end
+  def ensure_atom( atom) when is_atom(atom) do
+   atom
+  end
+  @doc """
+    returns
+    - the atom if `parameter` is already an atom
+    - `String.to_existing_atom(parameter)` if it is a `bitstring`
+  """
+  def ensure_existing_atom( string) when is_bitstring(string) do
+    String.to_existing_atom string
+  end
+  def ensure_existing_atom( atom) when is_atom(atom) do
+   atom
+  end
+
+end
