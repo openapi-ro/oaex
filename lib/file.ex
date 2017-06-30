153174b9ad9b4525ea47304a1cbd4f4443c2c15e
Paul Balomiri <paulbalomiri@gmail.com>
2017-06-30 18:18:29 +0300

    Added convenience `File.ensure_path/1` which creates the parent directories of a file

---
+++ lib/file.ex
@@ -0,0 +1,21 @@
+defmodule OA.File do
+  @doc """
+    ensures the parent directory of the file exists
+    if a trailing slash is contained, the directory is the `file` argument without the trailing separator
+    returns `{:ok, ensured_dir}` or `{:error, reason}`
+  """
+  def ensure_dir(file) do
+    dir = Path.dirname(file)
+    case File.mkdir_p(dir) do
+      :ok-> {:ok, dir}
+      {:error,_}=error-> error
+    end
+  end
+  @doc """
+  same as `ensure_dir/1` but trows on error.
+  """
+  def ensure_dir!(file) do
+    {:ok, ret} = ensure_dir(file)
+    file
+  end
+end
