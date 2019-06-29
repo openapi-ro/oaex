6643f8dc3c93023d231b3fd83ccba1410b28cee0
Paul Balomiri <paul@openapi.ro>
2019-06-29 15:22:27 +0300

    Add function to generate migration commands for composite foreign keys

--- lib/ecto/migration_helpers.ex
+++ lib/ecto/migration_helpers.ex
@@ -24,4 +24,46 @@ defmodule OA.Ecto.MigrationHelpers do
       ret
     end
   end
+  @doc """
+  creates a composite foreign key (with multiple keys).
+  """
+  def foreign_key(from_table, [_field1|_1]=from_fields, to_table, [_field2|_2]=to_fields, options ) do
+    [from_table, to_table] =
+      [from_table, to_table]
+      |> Enum.map(&to_string/1)
+    from_prefix =
+      case Keyword.get(options, :from_prefix) do
+        nil-> Keyword.get(options,:prefix,"")
+        ""->""
+        prefix-> "#{prefix}."
+      end
+      |> to_string()
+    to_prefix =
+      case Keyword.get(options, :to_prefix) do
+        nil-> Keyword.get(options,:prefix,"")
+        ""->""
+        prefix-> "#{prefix}."
+      end
+      |> to_string()
+    [from_prefix, to_prefix]=
+      [from_prefix, to_prefix]
+      |> Enum.map(&(&1 <> ".") )
+    from_fields =
+        from_fields
+        |>Enum.map( &to_string/1)
+        |>Enum.join(", ")
+    to_fields =
+      to_fields
+      |>Enum.map( &to_string/1)
+      |>Enum.join(", ")
+    fk_name = String.replace(from_prefix,".","_") <>
+      String.replace(from_fields, "," , "_") <> "_fk"
+    fk_name = String.replace(fk_name, " ", "")
+    Ecto.Migration.execute """
+    ALTER TABLE #{from_prefix}#{from_table}
+    ADD CONSTRAINT #{fk_name} FOREIGN KEY (#{from_fields})
+    REFERENCES #{to_prefix}#{to_table} (#{to_fields});
+    """,
+    "ALTER TABLE #{from_prefix}#{from_table} DROP CONSTRAINT #{fk_name}"
+  end
 end
